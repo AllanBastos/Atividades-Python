f0e2a25c0e5a07e2fac604d1aadea454b72bbf99
AllanBastos <allanbatistabastos@gmail.com>
2018-11-12 15:29:22 -0300

    transformando peça em em bjeto fixo

--- projeto/projeto.py
+++ projeto/projeto.py
@@ -3,12 +3,15 @@ import random
 
 def run():
     pygame.init()
+    # localização
     x = 360
     y = 5
-    tl = 50
-    ta = 30
 
-    tela = pygame.display.set_mode([1080, 720])
+
+    # tamanho
+
+
+    tela = pygame.display.set_mode([1100, 740])
     pygame.display.set_caption('TETRIS')
 
     relogio = pygame.time.Clock()
@@ -35,34 +38,39 @@ def run():
     limite_inferior = pygame.Rect(0, 715, 810, 5)
     limite_superior = pygame.Rect(0, 0, 810, 5)
 
+
+
+    # limite_esquerdo = pygame.Surface((5, 710))
+    # limite_direito = pygame.Surface((5, 710))
+    # limite_inferior = pygame.Surface((810, 5))
+    # limite_superior = pygame.Surface((810, 5))
+
     #pecas
 
-    peca1 = pygame.Rect(x, y, 90, 30)
+    peca1 = pygame.Rect(x, y, 30, 90)
     peca2 = pygame.Rect(x, y, 30, 30)
-    peca3 = pygame.Rect(x, y, 10, 10)
+    peca3 = pygame.Rect(x, y, 30, 85)
     pecas = [peca1, peca2, peca3]
     proxima_peca = random.choice(pecas)
     peca = proxima_peca
 
 
+
+
     sair = False
     while sair == False:
-        # if fim_da_tela == 0:
-        #     peca = proxima_peca
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sair = True
 
-
-
             if event.type == pygame.KEYDOWN and not peca.colliderect(limite_inferior):
                 if event.key == pygame.K_LEFT and not peca.colliderect(limite_esquerdo):
-                    peca.move_ip(-ta, 0)
+                    peca.move_ip(-30, 0)
                 if event.key == pygame.K_RIGHT and not peca.colliderect(limite_direito):
-                    peca.move_ip(ta, 0)
+                    peca.move_ip(30, 0)
                 if event.key == pygame.K_UP:
-                    peca = pygame.Rect(peca.centerx, peca.centery, peca.h, peca.w)
+                    peca = pygame.Rect(peca.x, peca.y, peca.height, peca.width)
 
 
         relogio.tick(29)
@@ -74,9 +82,11 @@ def run():
         pygame.draw.rect(tela, cor_amarela, limite_superior)
         pygame.draw.rect(tela, cor_amarela, limite_inferior)
 
+
         pygame.draw.rect(tela, cor_vermelha, peca)
 
         y = 5
+
 
 
 
@@ -85,13 +95,18 @@ def run():
             peca = peca.move(0, y)
             y += 5
 
+        altp, larp = peca.width, peca.height
 
 
-        # if peca.colliderect(limite_inferior):
-        #     upx, upy = peca.h, peca.w
-        #     peca = pygame.Surface((peca.width, peca.height))
-        #     peca.fill(cor_verde)
-        #     tela.blit(peca, [upx, upy])
+        peca_fixa = pygame.Rect(peca.x, peca.y, altp, larp)
+
+        if peca.colliderect(limite_inferior):
+            peca = pygame.draw.rect(tela, cor_verde, peca_fixa)
+
+
+
+
+
         pygame.display.update()
 
 
