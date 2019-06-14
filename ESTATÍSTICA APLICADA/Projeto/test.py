b4009dd03744d5ea810d056496ab5ba7a1b405a6
allanbastos <allabatistabastos@gmail.com>
2019-06-14 16:49:31 -0300

    criando caixas

--- ESTATÍSTICA APLICADA/Projeto/test.py
+++ ESTATÍSTICA APLICADA/Projeto/test.py
@@ -11,29 +11,24 @@ app.layout = html.Div([
     html.Label('Selecione Categoria'),
     dcc.Dropdown(
         options=[
-            {'label': 'Quantidade de acidentes', 'value': '2007'},
-            {'label': 'Montréal', 'value': 'MTL'},
-            {'label': u'San Francisco', 'value': 'SF'}
+            {'label': 'Quantidade de acidentes', 'value': 'qnt_a'},
+            {'label': 'Estado', 'value': 'uf'}
+
+
         ],
-        value='MTL'
+        value='qnt_a'
     ),
 
     html.Label('SELECIONE O(S) ANO(S)'),
     dcc.Dropdown(
         options=[
-            {'label': '2008', 'value': '2008'},
-            {'label': '2009', 'value': '2009'},
-            {'label': '2010', 'value': '2010'},
-            {'label': '2011', 'value': '2011'},
-            {'label': '2012', 'value': '2012'},
-            {'label': '2013', 'value': '2013'},
             {'label': '2014', 'value': '2014'},
             {'label': '2015', 'value': '2015'},
             {'label': '2016', 'value': '2016'},
             {'label': '2017', 'value': '2017'},
             {'label': '2018', 'value': '2018'}
         ],
-        value='MTL',
+        value='2014',
         multi=True
 
     ),
