d05f84fe3f75c1f6faa8f3457d340686719e37e1
allanbastos <allabatistabastos@gmail.com>
2019-06-14 15:39:45 -0300

    Criando arquivos

---
+++ ESTATÍSTICA APLICADA/Projeto/Acidentes de transito.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import plotly.graph_objs as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+low_memory=False
+
+df1 = pd.read_csv('csv/datatran2007.csv')
+df2 = pd.read_csv('csv/datatran2008.csv')
+df3 = pd.read_csv('csv/datatran2009.csv')
+df4 = pd.read_csv('csv/datatran2010.csv')
+df5 = pd.read_csv('csv/datatran2011.csv')
+df6 = pd.read_csv('csv/datatran2012.csv')
+df7 = pd.read_csv('csv/datatran2013.csv')
+df8 = pd.read_csv('csv/datatran2014.csv')
+df9 = pd.read_csv('csv/datatran2015.csv')
+df10 = pd.read_csv('csv/datatran2016.csv')
+df11 = pd.read_csv('csv/datatran2017.csv')
+df12 = pd.read_csv('csv/datatran2018.csv')
+
+print(df1, df2, df3, df4)
+
+
