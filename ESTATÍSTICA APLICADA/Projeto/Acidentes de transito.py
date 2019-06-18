be9179283b1ac50c67f9023f2bf88a47d9f87f7f
AllanBastos <44783589+AllanBastos@users.noreply.github.com>
2019-06-18 18:10:03 -0300

    Add files via upload
    Funcionando

--- ESTATÍSTICA APLICADA/Projeto/Acidentes de transito.py
+++ ESTATÍSTICA APLICADA/Projeto/Acidentes de transito.py
@@ -4,23 +4,70 @@ import plotly.graph_objs as go
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.plotly as py
 
 
-low_memory=False
 
-df1 = pd.read_csv('csv/datatran2007.csv')
-df2 = pd.read_csv('csv/datatran2008.csv')
-df3 = pd.read_csv('csv/datatran2009.csv')
-df4 = pd.read_csv('csv/datatran2010.csv')
-df5 = pd.read_csv('csv/datatran2011.csv')
-df6 = pd.read_csv('csv/datatran2012.csv')
-df7 = pd.read_csv('csv/datatran2013.csv')
-df8 = pd.read_csv('csv/datatran2014.csv')
-df9 = pd.read_csv('csv/datatran2015.csv')
-df10 = pd.read_csv('csv/datatran2016.csv')
-df11 = pd.read_csv('csv/datatran2017.csv')
-df12 = pd.read_csv('csv/datatran2018.csv')
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+df = pd.read_csv('csv/datatranAll.csv', low_memory=False)
 
 
 
 
+trace1 = go.Bar(
+    x=['ano 2007'],
+    y=[len(df[df['ano']==2007])],
+    name='2007'
+)
+
+trace2 = go.Bar(
+    x=['ano 2008'],
+    y=[len(df[df['ano']==2008])],
+    name='2008'
+)
+
+trace3 = go.Bar(
+    x=['ano 2009'],
+    y=[len(df[df['ano']==2009])],
+    name='2009'
+)
+
+trace4 = go.Bar(
+    x=['ano 2010'],
+    y=[len(df[df['ano']==2010])],
+    name='2010'
+)
+
+
+data = [trace1, trace2, trace3, trace4]
+layout = go.Layout(barmode='group')
+
+fig = go.Figure(data=data, layout=layout)
+
+
+
+
+app.layout = html.Div(children=[
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': data,
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- ESTATÍSTICA APLICADA/Projeto/test.py
+++ ESTATÍSTICA APLICADA/Projeto/test.py
@@ -2,6 +2,41 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import pandas as pd
+
+
+def criar_grafico_bar(ano, df):
+    trace = []
+
+    if list != type(ano):
+        trace.append(go.Bar(x=[ano], y=[len(df[df['ano'] == ano])], name=ano))
+    else:
+        for i in range(len(ano)):
+            trace.append(
+                go.Bar(
+                    x=[(ano[i])],
+                    y=[len(df[df['ano'] == ano[i]])],
+                    name=(ano[i])),
+            )
+
+    return [
+        dcc.Graph(
+            id='example-graph',
+            figure={
+                'data': trace,
+                'layout': {
+                    'title': 'Numeros de acidentes por ano'
+                }
+            }
+        )
+    ]
+
+
+df = pd.read_csv('csv/datatranAll.csv', low_memory=False)
+
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -10,31 +45,49 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
     html.Label('Selecione Categoria'),
     dcc.Dropdown(
+        id='categorias',
         options=[
             {'label': 'Quantidade de acidentes', 'value': 'qnt_a'},
             {'label': 'Estado', 'value': 'uf'}
 
 
         ],
-        value='qnt_a'
+        value='uf'
     ),
 
     html.Label('SELECIONE O(S) ANO(S)'),
     dcc.Dropdown(
+        id='ano',
         options=[
-            {'label': '2014', 'value': '2014'},
-            {'label': '2015', 'value': '2015'},
-            {'label': '2016', 'value': '2016'},
-            {'label': '2017', 'value': '2017'},
-            {'label': '2018', 'value': '2018'}
+            {'label': '2007', 'value': 2007},
+            {'label': '2008', 'value': 2008},
+            {'label': '2009', 'value': 2009},
+            {'label': '2010', 'value': 2010},
+            {'label': '2011', 'value': 2011},
+            {'label': '2012', 'value': 2012},
+            {'label': '2013', 'value': 2013},
+            {'label': '2014', 'value': 2014},
+            {'label': '2015', 'value': 2015},
+            {'label': '2016', 'value': 2016},
+            {'label': '2017', 'value': 2017},
+            {'label': '2018', 'value': 2018}
         ],
         value='2014',
         multi=True
 
     ),
+    html.Div(id='minha-saida')
+
+])
 
 
-], style={'columnCount': 2})
+@app.callback(Output('minha-saida', 'children'),
+              [Input(component_id='categorias', component_property='value'),
+               Input(component_id='ano', component_property='value')])
+def update_output(categoria, ano):
+    if categoria == 'qnt_a':
+       return criar_grafico_bar(ano, df)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
